b822b630fc51289708e942673f3569add993d218
Stela Capsa <stela.capsa@gmail.com>
2019-12-07 14:57:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
-  puts " #{season.capitalize}:"
+  puts "#{season.capitalize}:"
         holiday each.do |name, supply|
   puts "#{name.to_s.split("_").collect{|split_namee| split_namee.capitalize}.join(" ")}: #{supply.join(", ")}"
   end
