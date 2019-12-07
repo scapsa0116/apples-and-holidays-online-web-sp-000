bcf04b5d89423ea43efba6af0aea088252ad4750
Stela Capsa <stela.capsa@gmail.com>
2019-12-07 14:50:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,7 +77,7 @@ end
 
 
 def all_holidays_with_bbq(holiday_hash)
-holiday_supplies.map do |season, holiday|
+holiday_hash.map do |season, holiday|
   holiday.map do |holiday_name, supplys|
    supplys.include?("BBQ") ? holiday_name : nill
   
